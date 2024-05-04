a2fee369605d454f7b1e533c09defaef02d38843
Anastasia Lyah <lyahanastasya@gmail.com>
2024-05-04 22:33:00 +0300

    Add project files.

---
+++ SQL_Example/DBUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace SQL_Example
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "localhost";
+            int port = 3306;
+            string database = "world";
+            string username = "monty";
+            string password = "some_pass";
+
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+    }
+}
---
+++ SQL_Example/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+
+
+namespace SQL_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Отримати об'єкт Connection пфдключений до DB
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection conn = DBUtils.GetDBConnection();
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+                QueryEmployee(conn);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                //Закрити з'єднання
+                conn.Close();
+                //Знищити об'єкт, звільнити ресурс
+                conn.Dispose();
+            }
+            Console.Read();
+        }
+
+        public static void QueryEmployee(MySqlConnection conn)
+        {
+            string id, name, country;
+            string sql = "Select Code, Name, Continent from country";
+
+            //Створити об'єкт Command
+            MySqlCommand cmd = new MySqlCommand();
+
+            //Поєднувати Command з Connection
+            cmd.Connection = conn;
+            cmd.CommandText = sql;
+
+
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        id = reader["Code"].ToString();
+                        name = reader["Name"].ToString();
+                        country = reader["Continent"].ToString();
+                        Console.WriteLine("___________________________________");
+                        Console.WriteLine("Код: " + id + " Назва: " + name + " Континент: " + country);
+                        Console.WriteLine("___________________________________");
+                    }
+                }
+            }
+        }
+    }
+}
